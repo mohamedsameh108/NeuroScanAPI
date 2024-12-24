18a173771cbc413467d0afe78f7fd0a150900164
Mohamed Asem <mohamedasem318@gmail.com>
2024-12-24 20:01:49 +0000

    deleted old files

--- app.py
+++ app.py
@@ -14,8 +14,7 @@ CORS(app, resources={
         "origins": [
             "https://cerebroscan.netlify.app/",
             "http://localhost:3000",
-            "http://localhost:5173",
-            "http://localhost:5000",
+            "http://localhost:5173
             "http://127.0.0.1:5000"
         ], "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         "allow_headers": ["Content-Type", "Authorization"]
