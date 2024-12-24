00151c280a4bbff2c6637ced52f93789085502fd
Mohamed Sameh <samehmmm337@gmail.com>
2024-12-24 17:39:20 +0000

    remove last edit

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from tensorflow.keras.models import load_model
 
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "https://neuroscanweb.netlify.app"}})
+CORS(app)
 
 def image_processing(img):
     image_data = img.read()
