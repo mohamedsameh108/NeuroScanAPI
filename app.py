e584ab5ec4ff11e9351e5f0dde7b8cce6dac31f9
Mohamed Sameh <samehmmm337@gmail.com>
2024-12-24 18:53:41 +0000

    edit cors

--- app.py
+++ app.py
@@ -17,7 +17,9 @@ CORS(app, resources={
             "http://localhost:5173",
             "http://localhost:5000",
             "http://127.0.0.1:5000"
-        ]
+        ], "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        "allow_headers": ["Content-Type", "Authorization"]
+    }
     }
 })
 
