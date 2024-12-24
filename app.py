1ab575cc9e99c1cc15b1f35ee62d2321ba624404
Mohamed Asem <mohamedasem318@gmail.com>
2024-12-24 17:25:43 +0200

    Edit for deployment

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from preprocessing.preprocessing import image_processing
+from preprocessing import image_processing
 from tensorflow.keras.models import load_model
 import numpy as np
 import os
