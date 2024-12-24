b4bf5fb557872c56116ba2b947c77f04545d9043
Mohamed Asem <mohamedasem318@gmail.com>
2024-12-24 17:32:47 +0200

    Merging preprocessing file with main py file

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
+import cv2
+import numpy as np
+from PIL import Image
+import io
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from preprocessing import image_processing
 from tensorflow.keras.models import load_model
-import numpy as np
 import os
 
 app = Flask(__name__)
 CORS(app)
+
+def image_processing(img):
+    image_data = img.read()
+    image = Image.open(io.BytesIO(image_data))
+    img = np.array(image)
+
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_resized = cv2.resize(img_gray, (224,224))
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(7,7))
+    img_clahe = clahe.apply(img_resized)
+    norm_img = cv2.normalize(img_clahe, None, alpha=0.0, beta=1.0, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+    final_img = np.expand_dims(norm_img, axis=0)
+    return final_img
+    
+
 
 NonVSVeryMild_model = load_model(os.getcwd() + '\\models\\model_NonVSVeryMild.h5')
 NonVSMild_model = load_model(os.getcwd() + '\\models\\model_NonVSMild.h5')
